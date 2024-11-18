e6f859579b519e12b15ca7c256653e29d504a14e
Mykyta Melnyk <wawoxe@proton.me>
2024-11-18 21:40:50 +0100

    Added code style tools

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,5 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * (c) Mykyta Melnyk <wawoxe@proton.me>
+ *
+ * This software is licensed under the MIT License. See the LICENSE file for more details.
+ */
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
